8250b293c80fecfa549f3d55156c8d5fcb2d6135
PCDattt <datkg1097@gmail.com>
2026-09-21 21:26:58 +0700

    Fix unit test

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func sum(a, b int) int {
 func greet(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprintf(w, "Hello World! %s", time.Now())
 	if err != nil {
-		fmt.Println("Error :v", err)
+		fmt.Printf("Error %v\n", err)
 	}
 }
 
@@ -22,6 +22,6 @@ func main() {
 	http.HandleFunc("/", greet)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		fmt.Println("Error ListenAndServe %v", err)
+		fmt.Printf("Error ListenAndServe %v\n", err)
 	}
 }
